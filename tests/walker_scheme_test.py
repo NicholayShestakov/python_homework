86396bac179d3314f0e379e2a838d56f4e50ec32
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-12-15 15:23:54 +0300

    Update walker scheme test

--- tests/walker_scheme_test.py
+++ tests/walker_scheme_test.py
@@ -4,5 +4,6 @@ from rewriting_hw_1.walker_scheme import WalkerScheme
 def test_exception():
     try:
         scheme = WalkerScheme([("A", 0.1), ("B", 1)])
+        print(scheme)
     except Exception as e:
         assert str(e) == "Сумма вероятностей не равна 1."
